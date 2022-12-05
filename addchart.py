8487f25f8f80b9495da89e9b288f007e30d63cd9
emrecanakkaya <82708441+emrecanakkaya@users.noreply.github.com>
2022-12-05 03:56:18 +0100

    Create addchart.py

---
+++ addchart.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+# // amazon url and test data information
+wireless_device_url = 'https://www.amazon.com/HBN-Wireless-Weatherproof-Grounded-Included/dp/B082TX3CV1?ref_=Oct_DLandingS_D_606946ae_60'
+testuser = 'emreakkaya.nl@gmail.com'
+testpassword = 'Test123*'
+
+# // adding options . Because "sign in" menu is drop down menu
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+driver = webdriver.Chrome('C:\webdrivers\chromedriver.exe', chrome_options=options)
+action = ActionChains(driver)
+time.sleep(1)
+
+# // go to url
+driver.get(wireless_device_url)
+time.sleep(3)
+print("Amazon page is opened successfully")
+
+# // opening the drop down menu
+firstdropdownmenu = driver.find_element(by=By.XPATH,value='//*[@id="nav-link-accountList"]')
+action.move_to_element(firstdropdownmenu).perform()
+time.sleep(3)
+print("Dropdown menu is opened successfully ")
+
+# // go to url
+signinbutton = driver.find_element(by=By.XPATH,value='//*[@id="nav-flyout-ya-signin"]/a/span')
+signinbutton.click()
+time.sleep(3)
+print("Sign in page is opened successfully")
+
+# // This part is checking for links accessibility . but it failed couldn't understand the reason . just removed.
+### links = driver.find_elements(by=By.CSS_SELECTOR,value='a')
+#for link in links:
+    #r = requests.head(link.get_attribute('href'))
+    #print(link.get_attribute('href'), r.status_code)
+
+# // username
+username = driver.find_element(by=By.ID,value='ap_email')
+username.send_keys(testuser)
+cntbutton = driver.find_element(by=By.ID,value='continue')
+cntbutton.click()
+time.sleep(3)
+print("Username is entered successfully")
+
+# // password
+password = driver.find_element(by=By.ID,value='ap_password')
+password.send_keys(testpassword)
+signbutton = driver.find_element(by=By.ID,value='signInSubmit')
+signbutton.click()
+time.sleep(3)
+
+
+# // Adding assertion to check title and success login
+assert "Amazon" in driver.title
+print("Sucessfully login")
+
+addchart = driver.find_element(by=By.NAME,value='submit.add-to-cart')
+addchart.click()
+time.sleep(3)
+
+#alerttext = driver.switch_to.alert
+#alerttext = alerttext.text
+#print(alerttext)
+
+
+#totalprice = driver.find_element(by=By.NAME,value='ewcCurrencySymbol').get_attribute('value')
+#print(totalprice)
